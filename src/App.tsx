0da2eddab8487981a5d3d71f4abee9c274b2dfc3
virgiawankusuma <virgiawan150@gmail.com>
2023-10-22 14:05:11 +0700

    JJ-7 Slicing Manage User

--- src/App.tsx
+++ src/App.tsx
@@ -2,14 +2,19 @@ import { Routes, Route } from "react-router-dom";
 
 import Login from "./components/pages/Login";
 import FullLayout from "./components/layouts/FullLayout";
+import { AiOutlineDashboard, AiOutlineShop, AiOutlineUser, AiOutlineUserSwitch } from "react-icons/ai";
+
 import AdminList from "./components/pages/Admin/List";
-import { AiOutlineDashboard, AiOutlineShop } from "react-icons/ai";
 import AdminAdd from "./components/pages/Admin/Add";
 import AdminEdit from "./components/pages/Admin/Edit";
 import AdminDetail from "./components/pages/Admin/Detail";
 
+import CustomerList from "./components/pages/Customer/List";
+import CustomerAdd from "./components/pages/Customer/Add";
+import CustomerDetail from "./components/pages/Customer/Detail";
+import CustomerEdit from "./components/pages/Customer/Edit";
+
 export default function App() {
-  // const location = useLocation();
 
   return (
     <>
@@ -21,14 +26,24 @@ export default function App() {
             <FullLayout
               navList={[
                 {
-                  title: "Admin",
-                  link: "/admin",
+                  title: "Dashboard",
+                  link: "/dashboard",
                   icon: <AiOutlineDashboard />,
                 },
                 {
                   title: "Street Vendors",
                   link: "/vendors",
                   icon: <AiOutlineShop />,
+                },
+                {
+                  title: "Customers",
+                  link: "/customers",
+                  icon: <AiOutlineUser />,
+                },
+                {
+                  title: "Manage Admin",
+                  link: "/admin",
+                  icon: <AiOutlineUserSwitch />,
                 },
               ]}
             />
@@ -39,6 +54,12 @@ export default function App() {
             <Route path="add" element={<AdminAdd />} />
             <Route path="edit/:id" element={<AdminEdit />} />
             <Route path=":id" element={<AdminDetail />} />
+          </Route>
+          <Route path="/customers">
+            <Route index element={<CustomerList />} />
+            <Route path="add" element={<CustomerAdd />} />
+            <Route path="edit/:id" element={<CustomerEdit />} />
+            <Route path=":id" element={<CustomerDetail />} />
           </Route>
         </Route>
       </Routes>
