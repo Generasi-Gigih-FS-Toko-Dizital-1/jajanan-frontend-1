86975c529b2248f9437403ceb6e6fbb9d255c1a3
ridhoafwani <70614134+ridhoafwani@users.noreply.github.com>
2023-10-25 07:48:42 +0000

    JJ-40 Added list page

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,12 @@
 import { Routes, Route } from "react-router-dom";
 
 import FullLayout from "./components/layouts/FullLayout";
-import { AiOutlineDashboard, AiOutlineShop, AiOutlineUser, AiOutlineUserSwitch } from "react-icons/ai";
+import {
+	AiOutlineDashboard,
+	AiOutlineShop,
+	AiOutlineUser,
+	AiOutlineUserSwitch,
+} from "react-icons/ai";
 
 import Login from "./components/pages/Login";
 import Dashboard from "./components/pages/Dashboard";
@@ -22,61 +27,71 @@ import VendorDetail from "./components/pages/Vendor/Detail";
 import VendorEdit from "./components/pages/Vendor/Edit";
 
 export default function App() {
-
-  return (
-    <>
-      <Routes>
-        <Route path="/login" element={<Login />} />
-        <Route
-          path="/"
-          element={
-            <FullLayout
-              navList={[
-                {
-                  title: "Dashboard",
-                  link: "/dashboard",
-                  icon: <AiOutlineDashboard />,
-                },
-                {
-                  title: "Street Vendors",
-                  link: "/vendors",
-                  icon: <AiOutlineShop />,
-                },
-                {
-                  title: "Customers",
-                  link: "/customers",
-                  icon: <AiOutlineUser />,
-                },
-                {
-                  title: "Manage Admin",
-                  link: "/admin",
-                  icon: <AiOutlineUserSwitch />,
-                },
-              ]}
-            />
-          }
-        >
-          <Route path="/dashboard" element={<Dashboard />} />
-          <Route path="/admin">
-            <Route index element={<AdminList />} />
-            <Route path="add" element={<AdminAdd />} />
-            <Route path="edit/:id" element={<AdminEdit />} />
-            <Route path=":id" element={<AdminDetail />} />
-          </Route>
-          <Route path="/customers">
-            <Route index element={<CustomerList />} />
-            <Route path="add" element={<CustomerAdd />} />
-            <Route path="edit/:id" element={<CustomerEdit />} />
-            <Route path=":id" element={<CustomerDetail />} />
-          </Route>
-          <Route path="/vendors">
-            <Route index element={<VendorList />} />
-            <Route path="add" element={<VendorAdd />} />
-            <Route path="edit/:id" element={<VendorEdit />} />
-            <Route path=":id" element={<VendorDetail />} />
-          </Route>
-        </Route>
-      </Routes>
-    </>
-  );
+	return (
+		<>
+			<Routes>
+				<Route path="/login" element={<Login />} />
+				<Route
+					path="/"
+					element={
+						<FullLayout
+							navList={[
+								{
+									title: "Dashboard",
+									link: "/dashboard",
+									icon: <AiOutlineDashboard />,
+								},
+								{
+									title: "Street Vendors",
+									link: "/vendors",
+									icon: <AiOutlineShop />,
+								},
+								{
+									title: "Customers",
+									link: "/customers",
+									icon: <AiOutlineUser />,
+								},
+								{
+									title: "Manage Admin",
+									link: "/admin",
+									icon: <AiOutlineUserSwitch />,
+								},
+								{
+									title: "Top Up History",
+									link: "/top-up-history",
+									icon: <AiOutlineUserSwitch />,
+								},
+							]}
+						/>
+					}
+				>
+					<Route path="/dashboard" element={<Dashboard />} />
+					<Route path="/admin">
+						<Route index element={<AdminList />} />
+						<Route path="add" element={<AdminAdd />} />
+						<Route path="edit/:id" element={<AdminEdit />} />
+						<Route path=":id" element={<AdminDetail />} />
+					</Route>
+					<Route path="/customers">
+						<Route index element={<CustomerList />} />
+						<Route path="add" element={<CustomerAdd />} />
+						<Route path="edit/:id" element={<CustomerEdit />} />
+						<Route path=":id" element={<CustomerDetail />} />
+					</Route>
+					<Route path="/vendors">
+						<Route index element={<VendorList />} />
+						<Route path="add" element={<VendorAdd />} />
+						<Route path="edit/:id" element={<VendorEdit />} />
+						<Route path=":id" element={<VendorDetail />} />
+					</Route>
+					<Route path="/top-up-history">
+						<Route index element={<VendorList />} />
+						<Route path="add" element={<VendorAdd />} />
+						<Route path="edit/:id" element={<VendorEdit />} />
+						<Route path=":id" element={<VendorDetail />} />
+					</Route>
+				</Route>
+			</Routes>
+		</>
+	);
 }
