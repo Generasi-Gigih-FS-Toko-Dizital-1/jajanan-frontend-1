7c2a307765058451a008ebe40419aa7fecfaff5a
virgiawankusuma <virgiawan150@gmail.com>
2023-11-03 20:47:24 +0700

    JJ-5 Integrate Authentications API: Add admin authentication and logout functionality to Header component

--- src/components/layouts/Header.tsx
+++ src/components/layouts/Header.tsx
@@ -1,8 +1,36 @@
+import React from 'react'
 import { Avatar, Dropdown, DropdownItem, DropdownMenu, DropdownTrigger } from '@nextui-org/react'
 import { AiOutlineMenuFold, AiOutlineUser } from 'react-icons/ai'
-import React from 'react'
+
+import { useNavigate } from 'react-router-dom'
+import useFetch from '../../hooks/useFetch'
+import BackendOneClient from '../../clients/BackendOneClient'
+import useAuthentication from '../../hooks/useAuthentication'
 
 const Header = ({ sidebarToggle }: { sidebarToggle: () => void }): React.ReactElement => {
+  const navigate = useNavigate()
+  const client = new BackendOneClient()
+  const { authentication, setAuthentication }: any = useAuthentication()
+
+  const session = authentication.session
+
+  const url = `api/v1/admins/${session.accountId}`
+  const { data } = useFetch(url)
+
+  const handleLogout = (): void => {
+    confirm('Are you sure you want to logout?')
+      ? client.instance.post('api/v1/authentications/admins/logout', {
+        session: authentication.session
+      })
+        .then(() => {
+          setAuthentication(null)
+          localStorage.removeItem('authentication')
+          navigate('/login')
+        })
+        .catch((err: any) => { console.log(err) })
+      : alert('logout canceled')
+  }
+
   return (
     <header className="fixed w-full z-[99] flex p-4 items-center justify-between bg-[#FDD671]">
       <AiOutlineMenuFold
@@ -23,13 +51,19 @@ const Header = ({ sidebarToggle }: { sidebarToggle: () => void }): React.ReactEl
           className="text-center flex flex-col gap-2"
           onAction={(key) => {
             if (key === 'logout') {
-              confirm('Are you sure you want to logout?')
-              alert('admin succesfully logout')
+              handleLogout()
+            } else if (key === 'profile') {
+              navigate(`/admins/${session.accountId}`)
             }
           }
         }
         >
-          <DropdownItem>ADMIN 1</DropdownItem>
+          <DropdownItem
+            key="profile"
+            color="default"
+          >
+            <p className="text-jajanDark2">{data?.data.fullName}</p>
+          </DropdownItem>
           <DropdownItem
             key="logout"
             className="text-white bg-jajanDanger2/80"
